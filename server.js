9ecd5295b21b90ff432cf90999ffcf6adb20d576
mynameisaist <75547734+nameaist@users.noreply.github.com>
2024-03-09 02:02:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const app = require('./app');
-const dotenv = require('dotenv');
 
-dotenv.config({path: './config.env'});
-
-const port = process.env.PORT || 80;
+const port = 3000;
 const server = app.listen(port , () => {
     console.log(`App running on port ${port}`);
 })
